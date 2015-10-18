c1423a63f963c4a9daed88bb0a9244ec5644c6f2
dieter <dieter@frankfurt>
2015-10-18 20:55:40 +0200

    small updates on structure

--- pytools/src/IndexEval/fetchdata.py
+++ pytools/src/IndexEval/fetchdata.py
@@ -11,7 +11,7 @@ import collections
 import pymongo
 from pymongo.mongo_client import MongoClient
 
-from indexdata import IndexData
+from indexdata import IndexData, IndexHistory
 
 class FetchData():
     '''
@@ -26,21 +26,20 @@ class FetchData():
         self.indexName = indexName
         self.startDate = datetime.datetime(1900, 01, 01)
         self.endDate = datetime.datetime.today()
-                
+
     def fetchData(self):
         self.client = MongoClient()
         self.database = self.client[self.dbName]
         self.collection = self.database[self.indexName]
-        resultHistory = collections.deque()
-        
-        for entry in self.collection.find({'date': {'$gte': self.startDate, '$lt': self.endDate} }).sort('date'):    
+        history = IndexHistory()
+
+        for entry in self.collection.find({'date': {'$gte': self.startDate, '$lt': self.endDate} }).sort('date'):
             indexEntry = IndexData()
-            indexEntry.set(entry['date'], entry['open'], entry['close'], entry['high'], entry['low'])
-            indexEntry.setMeanValues(entry['mean5'], entry['mean13'],entry['mean38'], entry['mean89'], entry['mean200'])
-            resultHistory.append(indexEntry)
-         
-        return resultHistory
-    
+            indexEntry.setDictionary(entry)
+            history.addIndexData(indexEntry)
+
+        return history
+
     def fetchDataByDate(self, startDate, endDate):
         self.startDate = startDate
         self.endDate = endDate
@@ -49,7 +48,6 @@ class FetchData():
 if __name__ == '__main__':
     start = datetime.datetime(1998, 01, 02, 0, 0);
     end = datetime.datetime(1998, 02, 01, 0, 0)
-                                       
+
     fetchData = FetchData('stockdb', 'dax',)
-    fetchData.fetchDataByDate( start, end ) 
-           
+    fetchData.fetchDataByDate( start, end )
--- pytools/src/IndexEval/indexdata.py
+++ pytools/src/IndexEval/indexdata.py
@@ -8,15 +8,27 @@ import collections
 import csv
 import datetime
 import string
-   
+import indexdata
+
 
 class MeanSet:
-    
+    '''
+    MeanSet defines a set of mean values used for index analysis.
+    Currently there are mean values defined for
+        5 days
+        13 days
+        38 days
+        89 days and
+        200 days
+    '''
     class MeanData:
+        '''
+        MeanData is a helper class to calculate the mean value.
+        '''
         def __init__(self, size):
             self.meanSize = size
             self.valueQueue = collections.deque()
-            self.meanValue = 0       
+            self.meanValue = 0
 
         def addValue(self, value):
             if len(self.valueQueue) < self.meanSize:
@@ -26,28 +38,32 @@ class MeanSet:
                 self.meanValue = self.meanValue - self.valueQueue.popleft()
                 self.valueQueue.append(value)
                 self.meanValue = self.meanValue + value
-            
+
         def getMeanValue(self):
             if len(self.valueQueue) == self.meanSize:
                 return self.meanValue / self.meanSize
             else:
                 return 0
-                 
+
     def __init__(self):
         self.mean5 = self.MeanData(5)
         self.mean13 = self.MeanData(13)
         self.mean38 = self.MeanData(38)
-        self.mean89 = self.MeanData(89)        
+        self.mean89 = self.MeanData(89)
         self.mean200 = self.MeanData(200)
-    
+
     def setData(self, value):
         self.mean5.addValue( value )
         self.mean13.addValue( value )
         self.mean38.addValue( value )
         self.mean89.addValue( value )
-        self.mean200.addValue( value ) 
+        self.mean200.addValue( value )
 
 class IndexData:
+    '''
+    IndexData defines the data of one day which is used to analyze an index.
+    It holds the date, the values of the day and the corresponding mean values.
+    '''
     def __init__(self):
         self.date = datetime.date.today()
         self.open = 0
@@ -58,14 +74,14 @@ class IndexData:
         self.mean13 = 0
         self.mean38 = 0
         self.mean89 = 0
-        self.mean200 = 0 
+        self.mean200 = 0
 
     def _checkData(self):
         if self.low > self.high:
             temp = self.low
             self.low = self.high
             self.high = temp
-              
+
     def set(self, idxDate, idxOpen, idxClose, idxHigh, idxLow ):
         self.date = idxDate
         self.open = idxOpen
@@ -73,7 +89,7 @@ class IndexData:
         self.high = idxHigh
         self.low = idxLow
         self._checkData()
-    
+
 
     def setMeanValues(self, mean5, mean13, mean38, mean89, mean200 ):
         self.mean5 = mean5
@@ -81,14 +97,14 @@ class IndexData:
         self.mean38 = mean38
         self.mean89 = mean89
         self.mean200 = mean200
-                
+
     def setMean(self, mean):
         self.mean5 = mean.mean5.getMeanValue()
         self.mean13 = mean.mean13.getMeanValue()
         self.mean38 = mean.mean38.getMeanValue()
         self.mean89 = mean.mean89.getMeanValue()
         self.mean200 = mean.mean200.getMeanValue()
-        
+
     def getDictionary(self):
         return { "date": datetime.datetime(self.date.year,
                                            self.date.month,
@@ -102,7 +118,7 @@ class IndexData:
                  "mean38": self.mean38,
                  "mean89": self.mean89,
                  "mean200": self.mean200 }
-        
+
     def setDictionary(self, data):
         self.date = data["date"]
         self.open = data["open"]
@@ -117,17 +133,19 @@ class IndexData:
         self._checkData()
 
 class IndexHistory:
-    def __init__(self, fileName, historyLimit = 0):
-        self.fileName = fileName
-        self.historyLimit = historyLimit
-        self.indexHistory = collections.deque()       
-        
-    def readIndex(self):
+    '''
+    IndexHistory defines the history of a stock index. For each available day there is one
+    entry stored in a queue.
+    '''
+    def __init__(self):
+        self.indexHistory = collections.deque()
+
+    def readIndex(self, fileName, historyLimit = 0):
         with open(self.fileName, "rb") as iFile:
             csvReader = csv.DictReader(iFile, ['Date', 'Open', 'High', 'Low', 'Close'])
             for row in csvReader:
                 current = IndexData()
-                currentDate = string.split(row['Date'], '.')            
+                currentDate = string.split(row['Date'], '.')
                 current.set( datetime.date(int(currentDate[2]), int(currentDate[1]), int(currentDate[0])),
                              float(row['Open']),
                              float(row['Close']),
@@ -136,31 +154,43 @@ class IndexHistory:
                 self.indexHistory.append( current )
                 if (self.historyLimit > 0) and (len(self.indexHistory) > self.historyLimit):
                     break
-                
-                
+
+
         self.indexHistory.reverse()
-        
+        self.calcMeanValues()
+
+    def addIndexData(self, data):
+        self.indexHistory.append(data)
+
+    def calcMeanValues(self):
         meanSet = MeanSet()
         for indexData in self.indexHistory:
             meanSet.setData(indexData.close)
-            indexData.setMean( meanSet )
-            
-    def addIndexData(self, data):
-        self.indexHistory.append(data)
-        
-        
+            indexData.setMean(meanSet)
+
+class IndexResult:
+    '''
+    IndexResult defines the result of one transaction, together with the historical data
+    entries between buy and sell.
+    '''
+    def __init__(self, indexBuy, indexSell, indexHistory, knockOut ):
+        self.indexBuy = indexBuy
+        self.indexSell = indexSell
+        self.indexHistory = indexHistory
+        self.knockOut = knockOut
+
 class IndexResultHistory:
+    '''
+    IndexResultHistory defines the history of all transactions (index results) done for
+    a period.
+    '''
     def __init__(self):
-        self.winCount = 0
-        self.looseCount = 0
-        
         self.resultHistory = collections.deque()
-        
+
     def addIndexResult(self, result):
         self.resultHistory.append(result)
-                        
+
     def evaluateResult(self, result):
         for indexResult in self.resultHistory:
             result.evaluate( indexResult )
-                                
-        
+
